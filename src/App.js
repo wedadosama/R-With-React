f9ef69f37a3da123c5e5d02778805f13f04fcb18
Wedad Osama <welkammash@gmail.com>
2025-02-19 19:15:32 +0200

    Add:Section Services

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Header from "./components/layouts/Header";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <About/>
+      <About />
+      <Services />
     </>
   );
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,35 @@
+function Services() {
+  return (
+    <>
+      <section id="services" className="text-center mt-24">
+        <div className="container">
+          <div className="service">
+            <h2 className="mb-5 text-6xl font-bold">services</h2>
+            <p className="col my-5 leading-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin <br />
+              tristique elit nec malesuada tincidunt. Pellentesque lobortis Web Development
+            </p>
+          </div>
+          <div className="cards cards flex flex-wrap justify-center items-center gap-5">
+            <div className="card card w-64 p-5 mt-12 rounded-lg text-white bg-gray-800">
+              <img src="icons/web-development.webp" alt="development" className="mb-5" />
+              <h2 className="mb-5 text-xl font-bold">Web Development</h2>
+              <p className="my-5 leading-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique elit nec</p>
+            </div>
+            <div className="card card w-64 p-5 mt-12 rounded-lg text-white bg-gray-800">
+              <img src="icons/web-design.webp" alt="desgin"className="mb-5" />
+              <h2 className="mb-5 text-xl font-bold">Web Design</h2>
+              <p className="my-5 leading-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique elit nec</p>
+            </div>
+            <div className="card card w-64 p-5 mt-12 rounded-lg text-white bg-gray-800">
+              <img src="icons/mobile.webp" alt="mobile" className="mb-5" />
+              <h2 className="mb-5 text-xl font-bold">APP Design</h2>
+              <p className="my-5 leading-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique elit nec</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+export default Services;
