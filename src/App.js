c9f0edfd6b8fd62a88da3bb7bc98591a1c531c40
Wedad Osama <welkammash@gmail.com>
2025-02-19 21:15:33 +0200

    Add: Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Blog from "./components/Blog";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
+import Footer from "./components/layouts/Footer";
 import Header from "./components/layouts/Header";
 import Services from "./components/Services";
 
@@ -14,6 +15,7 @@ function App() {
       <Services />
       <Blog />
       <Contact />
+      <Footer/>
     </>
   );
 }
---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+  return(
+    <>
+    <footer className="bg-[#212529] text-center p-6 text-white font-bold">
+    <div className="container">
+      <p className="">@RidhoSatriawan</p>
+    </div>
+    </footer> 
+    </>
+  )
+  
+}
+export default Footer;
